54a8c151ec4a995eb7ba3997cfdf630a6bb55b76
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2024-03-20 13:55:24 +0100

    Update test_minimal_client_sync_write_lock.rs

--- crates/minimal-client-sync-write-lock/tests/test_minimal_client_sync_write_lock.rs
+++ crates/minimal-client-sync-write-lock/tests/test_minimal_client_sync_write_lock.rs
@@ -17,29 +17,15 @@ fn test_thor_minimal_client_sync_1000_st() {
 #[test]
 fn test_thor_minimal_client_sync_1000_mt() {
     let func1 = CString::new("Function1").unwrap();
-    let func2 = CString::new("Function2").unwrap();
-    let func3 = CString::new("Function3").unwrap();
-    let func4 = CString::new("Function4").unwrap();
 
-    let handles = (0..250)
+    let handles = (0..8)
         .map(|_| {
             let func1 = func1.clone();
-            let func2 = func2.clone();
-            let func3 = func3.clone();
-            let func4 = func4.clone();
-
             std::thread::spawn(move || {
-                unsafe { start_rapl(func1.as_ptr()) };
-                unsafe { stop_rapl(func1.as_ptr()) };
-
-                unsafe { start_rapl(func2.as_ptr()) };
-                unsafe { stop_rapl(func2.as_ptr()) };
-
-                unsafe { start_rapl(func3.as_ptr()) };
-                unsafe { stop_rapl(func3.as_ptr()) };
-
-                unsafe { start_rapl(func4.as_ptr()) };
-                unsafe { stop_rapl(func4.as_ptr()) };
+                for _ in 0..1000 {
+                    unsafe { start_rapl(func1.as_ptr()) };
+                    unsafe { stop_rapl(func1.as_ptr()) };
+                }
             })
         })
         .collect::<Vec<_>>();
