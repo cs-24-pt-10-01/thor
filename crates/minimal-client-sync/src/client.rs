c65653e3584a63f0d35911848c7e6c472ff7c38a
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2024-03-20 13:06:50 +0100

    almost done impl for minimal client sync

--- crates/minimal-client-sync/src/client.rs
+++ crates/minimal-client-sync/src/client.rs
@@ -1,34 +1,31 @@
 use crossbeam::queue::SegQueue;
+use csv::{Writer, WriterBuilder};
+use serde::Serialize;
 use std::{
+    fs::{File, OpenOptions},
     io::Write,
     net::TcpStream,
     sync::Once,
     thread,
-    time::{Duration, SystemTime},
+    time::{Duration, SystemTime, UNIX_EPOCH},
 };
+use thor_lib::{read_rapl_msr_registers, RaplMeasurement};
 use thor_shared::{ConnectionType, LocalClientPacket, LocalClientPacketOperation};
 
-static RAPL_INIT: Once = Once::new();
-
 static LOCAL_CLIENT_PACKET_QUEUE: SegQueue<LocalClientPacket> = SegQueue::new();
+static mut CSV_WRITER: Option<Writer<File>> = None;
 
 pub fn start_rapl(id: impl AsRef<str>) {
-    // Initialize RAPL
-    RAPL_INIT.call_once(|| {
-        thread::spawn(send_packet_queue);
-    });
+    let rapl_registers = read_rapl_msr_registers();
 
-    let packet = LocalClientPacket {
-        id: id.as_ref().to_string(),
-        process_id: 12345,
-        thread_id: thread_id::get(),
-        operation: LocalClientPacketOperation::Start,
-        timestamp: SystemTime::now()
-            .duration_since(std::time::UNIX_EPOCH)
-            .unwrap()
-            .as_millis(),
-    };
-    LOCAL_CLIENT_PACKET_QUEUE.push(packet);
+    match rapl_registers {
+        RaplMeasurement::Intel(intel) => {
+            write_to_csv(intel, vec!["pp0", "pp1", "pkg", "dram"]).unwrap();
+        }
+        RaplMeasurement::AMD(amd) => {
+            write_to_csv(amd, vec!["core", "pkg"]).unwrap();
+        }
+    }
 }
 
 pub fn stop_rapl(id: impl AsRef<str>) {
@@ -45,24 +42,46 @@ pub fn stop_rapl(id: impl AsRef<str>) {
     LOCAL_CLIENT_PACKET_QUEUE.push(packet);
 }
 
-fn send_packet_queue() {
-    // Connect to the server
-    let mut stream = TcpStream::connect("127.0.0.1:6969").unwrap();
+fn write_to_csv<T, C, U>(data: T, columns: C) -> Result<(), std::io::Error>
+where
+    T: Serialize,
+    C: IntoIterator<Item = U>,
+    U: AsRef<[u8]>,
+{
+    let wtr = match unsafe { CSV_WRITER.as_mut() } {
+        Some(wtr) => wtr,
+        None => {
+            // Open the file to write to CSV. First argument is CPU type, second is RAPL power units
+            let file = OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(format!("test.csv",))?;
 
-    // Signify which type of client this is (it is a local client)
-    stream.write_all(&[ConnectionType::Local as u8]).unwrap();
+            // Create the CSV writer
+            let mut wtr = WriterBuilder::new().from_writer(file);
 
-    loop {
-        while let Some(packet) = LOCAL_CLIENT_PACKET_QUEUE.pop() {
-            // Serialize the packet using bincode
-            let serialized = bincode::serialize(&packet).unwrap();
+            // Write the column names
+            wtr.write_record(columns)?;
 
-            // Write length and then the serialized packet
-            stream.write_all(&[(serialized.len() as u8)]).unwrap();
-            stream.write_all(&serialized).unwrap();
+            // Store the CSV writer in a static variable
+            unsafe { CSV_WRITER = Some(wtr) };
+
+            // Return a mutable reference to the CSV writer
+            unsafe { CSV_WRITER.as_mut().expect("failed to get CSV writer") }
         }
+    };
 
-        // Sleep for 2 seconds
-        thread::sleep(Duration::from_secs(2));
-    }
+    // Write the data to the CSV and flush it
+    wtr.serialize(data)?;
+    wtr.flush()?;
+
+    Ok(())
+}
+
+fn get_timestamp_millis() -> u128 {
+    let current_time = SystemTime::now();
+    let duration_since_epoch = current_time
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    duration_since_epoch.as_millis()
 }
