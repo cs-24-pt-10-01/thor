2d7b6a7aa3cbb8a4f08b1d19a0fd2723e77470db
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2024-03-20 13:24:29 +0100

    rename

--- crates/minimal-client-sync-write-lock/tests/test_minimal_client_sync_write_lock.rs
+++ crates/minimal-client-sync-write-lock/tests/test_minimal_client_sync_write_lock.rs
@@ -2,7 +2,7 @@ use std::ffi::CString;
 use thor_minimal_client_sync::ffi::{start_rapl, stop_rapl};
 
 #[test]
-fn test_send_function() {
+fn test_thor_minimal_client_sync() {
     let func1 = CString::new("Function1").unwrap();
     unsafe { start_rapl(func1.as_ptr()) };
     unsafe { stop_rapl(func1.as_ptr()) };
