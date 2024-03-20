99c0c26eaa195d7159197065a9e7481fb7b00368
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2024-03-20 13:37:40 +0100

    utilize 1000 calls

--- crates/minimal-client-sync-write-lock/tests/test_minimal_client_sync_write_lock.rs
+++ crates/minimal-client-sync-write-lock/tests/test_minimal_client_sync_write_lock.rs
@@ -2,14 +2,17 @@ use std::ffi::CString;
 use thor_minimal_client_sync::ffi::{start_rapl, stop_rapl};
 
 #[test]
-fn test_thor_minimal_client_sync() {
+fn test_thor_minimal_client_sync_1000() {
     let func1 = CString::new("Function1").unwrap();
-    unsafe { start_rapl(func1.as_ptr()) };
-    unsafe { stop_rapl(func1.as_ptr()) };
-
     let func2 = CString::new("Function2").unwrap();
-    unsafe { start_rapl(func2.as_ptr()) };
-    unsafe { stop_rapl(func2.as_ptr()) };
+
+    for _ in 0..1000 {
+        unsafe { start_rapl(func1.as_ptr()) };
+        unsafe { stop_rapl(func1.as_ptr()) };
+
+        unsafe { start_rapl(func2.as_ptr()) };
+        unsafe { stop_rapl(func2.as_ptr()) };
+    }
 
     // Sleep for 5 seconds
     std::thread::sleep(std::time::Duration::from_secs(5));
