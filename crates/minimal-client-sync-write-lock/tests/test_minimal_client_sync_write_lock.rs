791e688ffca679b345d228a3a80eb2ac3e53350a
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2024-03-20 13:41:08 +0100

    Update test_minimal_client_sync_write_lock.rs

--- crates/minimal-client-sync-write-lock/tests/test_minimal_client_sync_write_lock.rs
+++ crates/minimal-client-sync-write-lock/tests/test_minimal_client_sync_write_lock.rs
@@ -4,14 +4,10 @@ use thor_minimal_client_sync::ffi::{start_rapl, stop_rapl};
 #[test]
 fn test_thor_minimal_client_sync_1000() {
     let func1 = CString::new("Function1").unwrap();
-    let func2 = CString::new("Function2").unwrap();
 
     for _ in 0..1000 {
         unsafe { start_rapl(func1.as_ptr()) };
         unsafe { stop_rapl(func1.as_ptr()) };
-
-        unsafe { start_rapl(func2.as_ptr()) };
-        unsafe { stop_rapl(func2.as_ptr()) };
     }
 
     // Sleep for 5 seconds
