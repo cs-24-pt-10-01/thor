3aef8732a9ea3371d63d4a975763c60e5cbac056
Jakob <31919330+Jakobzs@users.noreply.github.com>
2024-03-27 12:59:35 +0100

    Update ffi.rs

--- crates/minimal-client-empty/src/ffi.rs
+++ crates/minimal-client-empty/src/ffi.rs
@@ -1,13 +1,13 @@
-use std::ffi::{c_char, CStr};
+use std::ffi::c_char;
 
 // # Safety
 //
 // This function is unsafe because it dereferences the `id` pointer.
 #[no_mangle]
-pub unsafe extern "C" fn start_rapl(id: *const c_char) {}
+pub unsafe extern "C" fn start_rapl(_id: *const c_char) {}
 
 // # Safety
 //
 // This function is unsafe because it dereferences the `id` pointer.
 #[no_mangle]
-pub unsafe extern "C" fn stop_rapl(id: *const c_char) {}
+pub unsafe extern "C" fn stop_rapl(_id: *const c_char) {}
