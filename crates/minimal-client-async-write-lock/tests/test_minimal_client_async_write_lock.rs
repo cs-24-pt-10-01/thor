dfb2bd896c9800428589956187e6506d5fae7361
Jakob <31919330+Jakobzs@users.noreply.github.com>
2024-04-02 11:30:19 +0200

    Minimal client (#11)
    * prep separation for intel rapl reader
    
    * start on minimal client
    
    * separate to sync ver. prepare async-lock, async-lockfree
    
    * almost done impl for minimal client sync
    
    * rename dir and more impl
    
    * doc
    
    * Update client.rs
    
    * doc for examples
    
    * Create start_stop_test.rs
    
    * fix using as lib........
    
    * doc
    
    * rename
    
    * Update test_minimal_client_sync_write_lock.rs
    
    * impl for showing sync design now
    
    * utilize 1000 calls
    
    * Update test_minimal_client_sync_write_lock.rs
    
    * prepare mt example, use lock for writer
    
    * Update test_minimal_client_sync_write_lock.rs
    
    * doc
    
    * Update test_minimal_client_sync_write_lock.rs
    
    * add minimal client empty
    
    * rename test
    
    * add for async lock
    
    * add queue
    
    * attempt with spawning background writer on async
    
    * use string for the data accordingly
    
    * fix queue writing....
    
    * use diff buffer
    
    * use write once for record writing
    
    * start impl for lockfree
    
    * remake file
    
    * adjust for lockfree queue
    
    * Update ffi.rs
    
    * removed warnings for minimal clients
    
    * Update test_minimal_client_async_write_lock.rs
    
    * A readme for the crates folder
    
    * run tests under sudo
    
    * add st and mt for flags
    
    * add doc for running tests
    
    * test for 100000 executions
    
    * awer
    
    * reduce count to 10k for iter
    
    * fix name
    
    * awer
    
    ---------
    
    Co-authored-by: Villiam <Vjacob19@student.aau.dk>

---
+++ crates/minimal-client-async-write-lock/tests/test_minimal_client_async_write_lock.rs
@@ -0,0 +1,41 @@
+use std::ffi::CString;
+use thor_minimal_client_async_lock::ffi::{start_rapl, stop_rapl};
+
+#[test]
+fn test_thor_minimal_client_async_lock_1000_st() {
+    let func1 = CString::new("FunctionST").unwrap();
+
+    for _ in 0..1000 {
+        unsafe { start_rapl(func1.as_ptr()) };
+        unsafe { stop_rapl(func1.as_ptr()) };
+    }
+
+    // sleep for 5 seconds
+    std::thread::sleep(std::time::Duration::from_secs(5));
+}
+
+// cargo test --release --package thor-minimal-client-async-lock --test test_minimal_client_async_write_lock -- test_thor_minimal_client_async_lock_1000_mt --exact --nocapture
+#[test]
+fn test_thor_minimal_client_async_lock_1000_mt() {
+    let func1 = CString::new("FunctionMT").unwrap();
+
+    // Test for 8 threads 1000 times
+    let handles = (0..8)
+        .map(|_| {
+            let func1 = func1.clone();
+            std::thread::spawn(move || {
+                for _ in 0..1000 {
+                    unsafe { start_rapl(func1.as_ptr()) };
+                    unsafe { stop_rapl(func1.as_ptr()) };
+                }
+            })
+        })
+        .collect::<Vec<_>>();
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    // sleep for 5 seconds
+    std::thread::sleep(std::time::Duration::from_secs(5));
+}
