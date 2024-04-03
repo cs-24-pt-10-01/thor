347a17d7c47324fa07b691d25d190ba724f7ecb4
Villiamfj <40019337+Villiamfj@users.noreply.github.com>
2024-04-03 10:53:50 +0200

    sequential shared lib (#12)
    * sequential shared lib
    
    * No need to sleep

---
+++ crates/shared-lib-sync/tests/start_stop_test.rs
@@ -0,0 +1,13 @@
+use std::ffi::CString;
+use thor_lib_sync::ffi::{start_rapl, stop_rapl};
+
+#[test]
+fn test_send_function() {
+    let func1 = CString::new("Function1").unwrap();
+    unsafe { start_rapl(func1.as_ptr()) };
+    unsafe { stop_rapl(func1.as_ptr()) };
+
+    let func2 = CString::new("Function2").unwrap();
+    unsafe { start_rapl(func2.as_ptr()) };
+    unsafe { stop_rapl(func2.as_ptr()) };
+}
