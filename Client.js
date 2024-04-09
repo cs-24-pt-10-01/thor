de318802db3d9448358c94c078b7b52798105321
Villiam <Vjacob19@student.aau.dk>
2024-04-09 11:24:02 +0200

    Changed end check for client.js

--- Client.js
+++ Client.js
@@ -6,7 +6,8 @@ const Net = require('net');
 // The port number and hostname of the server.
 const port = 6969;
 const host = "127.0.0.1";
-const end = "end";
+const endString = "end";
+const end = new Buffer.from(endString);
 const repo = "https://github.com/cs-24-pt-10-01/HotspotBenchmarkJS.git";
 
 const client = new Net.Socket();
@@ -20,7 +21,7 @@ client.connect({ port: port, host: host }, function () {
 });
 
 client.on('data', function (data) {
-    if (data.toString('utf-8').endsWith(end)) {
+    if (data.subarray(data.length - end.length).toString() == endString) {
         dataBuffer = Buffer.concat([dataBuffer, data]);
 
         const dataBufferString = dataBuffer.toString().slice(0, -end.length);
