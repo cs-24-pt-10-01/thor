e83ede477bd0e1da0455c7a0ff25bec604cc3be2
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2024-05-24 09:39:56 +0200

    simplify to funcs

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -4,6 +4,8 @@ use crate::{
 };
 use config::Config;
 use std::{fs, sync::Arc, thread::sleep};
+use thor_lib::{IntelRaplRegistersJoules, RaplMeasurementJoules};
+use thor_shared::{ClientPacket, ProcessUnderTestPacket, ProcessUnderTestPacketOperation};
 
 mod build;
 mod component_def;
@@ -12,7 +14,88 @@ mod listener;
 mod measurement;
 mod start_process;
 
+fn print_put_packet_len(put_packet: ProcessUnderTestPacket) {
+    let serialized_put = bincode::serialize(&put_packet).unwrap();
+    println!("serialized put len: {}", serialized_put.len());
+}
+
+fn print_client_packet_len(client_packet: &ClientPacket) {
+    let serialized_client = bincode::serialize(&client_packet).unwrap();
+    println!("serialized client len: {}", serialized_client.len());
+}
+
+fn print_client_packets_len(client_packets: &Vec<ClientPacket>) {
+    let serialized_client = bincode::serialize(&client_packets).unwrap();
+    println!("serialized clients len: {}", serialized_client.len());
+}
+
 fn main() {
+    print_put_packet_len(ProcessUnderTestPacket {
+        id: "".to_string(),
+        process_id: 0,
+        thread_id: 0,
+        operation: ProcessUnderTestPacketOperation::Start,
+        timestamp: 0,
+    });
+
+    print_put_packet_len(ProcessUnderTestPacket {
+        id: "AAAAA".to_string(),
+        process_id: 0,
+        thread_id: 0,
+        operation: ProcessUnderTestPacketOperation::Start,
+        timestamp: 0,
+    });
+    print_put_packet_len(ProcessUnderTestPacket {
+        id: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".to_string(),
+        process_id: 0,
+        thread_id: 0,
+        operation: ProcessUnderTestPacketOperation::Start,
+        timestamp: 0,
+    });
+
+    let client_packet = ClientPacket {
+        process_under_test_packet: ProcessUnderTestPacket {
+            id: "".to_string(),
+            process_id: 0,
+            thread_id: 0,
+            operation: ProcessUnderTestPacketOperation::Start,
+            timestamp: 0,
+        },
+        rapl_measurement: RaplMeasurementJoules::Intel(IntelRaplRegistersJoules {
+            pp0: 0.0,
+            pp1: 0.0,
+            pkg: 0.0,
+            dram: 0.0,
+        }),
+        pkg_overflow: 0,
+    };
+    serde_json::to_vec(&client_packet).unwrap();
+    print_client_packet_len(&client_packet);
+
+    let mut client_packets = Vec::new();
+    client_packets.push(client_packet);
+    print_client_packets_len(&client_packets);
+
+    let client_packet2 = ClientPacket {
+        process_under_test_packet: ProcessUnderTestPacket {
+            id: "".to_string(),
+            process_id: 0,
+            thread_id: 0,
+            operation: ProcessUnderTestPacketOperation::Start,
+            timestamp: 0,
+        },
+        rapl_measurement: RaplMeasurementJoules::Intel(IntelRaplRegistersJoules {
+            pp0: 0.0,
+            pp1: 0.0,
+            pkg: 0.0,
+            dram: 0.0,
+        }),
+        pkg_overflow: 0,
+    };
+
+    client_packets.push(client_packet2);
+    print_client_packets_len(&client_packets);
+
     //getting config
     let config_file_data =
         fs::read_to_string("thor-server.toml").expect("Failed to read thor-server.toml");
